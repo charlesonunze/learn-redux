fa844c8f2cca0dd3b583c1111690fea71b065b95
charlesonunze <charlesonunze2@gmail.com>
2020-07-15 03:31:03 +0100

    feat: 🎸 function composition

--- src/fp.js
+++ src/fp.js
@@ -19,3 +19,14 @@ greet(sayHello());
 setTimeout(() => console.log('Hello there!'), 1000);
 const evenNumbers = [1, 3, 2, 5, 4, 6].filter((num) => num % 2 === 0)
 console.log(evenNumbers);
+
+// Function Composition
+const input = '   JavaScript   ';
+const output = `<div>${input.trim()}</div>`; // Not functional
+
+const trim = str => str.trim();
+const wrapInDiv = str => `<div>${str}</div>`; // Funtional
+const result = wrapInDiv(trim(input)); // Function composition in action
+
+console.log('!functional', output);
+console.log('functional!', result);
