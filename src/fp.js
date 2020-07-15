70b2feea0921989e2d289b148b31980fc7a001d6
charlesonunze <charlesonunze2@gmail.com>
2020-07-15 11:10:54 +0100

    feat: 🎸 composing and piping with lodash

--- src/fp.js
+++ src/fp.js
@@ -1,5 +1,5 @@
 // Functional Programming
-
+const { compose, pipe } = require('lodash/fp')
 
 // 01. Higher Order Functions
 
@@ -20,7 +20,7 @@ setTimeout(() => console.log('Hello there!'), 1000);
 const evenNumbers = [1, 3, 2, 5, 4, 6].filter((num) => num % 2 === 0)
 console.log(evenNumbers);
 
-// Function Composition
+// 02. Function Composition
 const input = '   JavaScript   ';
 const output = `<div>${input.trim()}</div>`; // Not functional
 
@@ -30,3 +30,10 @@ const result = wrapInDiv(trim(input)); // Function composition in action
 
 console.log('!functional', output);
 console.log('functional!', result);
+
+// Composition with Lodash
+const composed = compose(wrapInDiv, trim);
+console.log('Lodash Compose', composed(input));
+
+const piedPiper = pipe(trim, wrapInDiv);
+console.log('Pied Piper!', piedPiper(input));
