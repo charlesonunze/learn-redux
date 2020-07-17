c544673d02517ad628394cac204429691fa34b1d
charlesonunze <charlesonunze2@gmail.com>
2020-07-17 19:38:16 +0100

    feat: 🎸 redux basics: reducers

---
+++ src/first-redux-app/reducer.js
@@ -0,0 +1,24 @@
+let lastId = 0;
+
+function reducer(state = [], action) {
+  switch (action.type) {
+    case 'ADD_BUG':
+      return [
+        ...state,
+        {
+          description: action.payload.description,
+          resolved: false,
+          id: ++lastId
+        }
+      ]
+      break;
+
+    case 'REMOVE_BUG':
+      return state.filter(b => b.id !== action.payload.id)
+      break;
+
+    default:
+      return state;
+      break;
+  }
+}
