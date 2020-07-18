ae43c490c3493bdd699ecb79e4779c66d6750eda
charlesonunze <charlesonunze2@gmail.com>
2020-07-18 11:34:49 +0100

    feat: 🎸 redux from scratch: subscribe

--- src/redux-from-scratch/index.js
+++ src/redux-from-scratch/index.js
@@ -1,6 +1,14 @@
 const store = require("./store");
 const { addBug, removeBug } = require("./actions");
 
+store.subscribe(() => {
+  console.log('LISTENER 1');
+});
+
+store.subscribe(() => {
+  console.log('LISTENER 2');
+});
+
 store.dispatch(addBug('New bug from our custom redux!'))
 console.log(store.getState());
 
--- src/redux-from-scratch/store.js
+++ src/redux-from-scratch/store.js
@@ -2,14 +2,19 @@ const reducer = require("./reducer");
 
 function createStore(reducer) {
   let state;
+  let listeners = [];
 
   return {
     dispatch: function (action) {
       state = reducer(state, action)
+
+      listeners.forEach(listener => {
+        listener()
+      });
     },
 
-    subscribe: function () {
-
+    subscribe: function (listener) {
+      listeners.push(listener)
     },
 
     getState: function () {
