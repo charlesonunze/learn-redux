c73cf11fb7a05bfd82c7a34f0fb011cde35fbddb
charlesonunze <charlesonunze2@gmail.com>
2020-07-19 21:50:42 +0100

    feat: 🎸 redux-toolkit: createReducer

--- src/02_FIRST_REDUX_APP/bugs.js
+++ src/02_FIRST_REDUX_APP/bugs.js
@@ -1,31 +1,23 @@
-import { createAction } from '@reduxjs/toolkit'
+import { createAction, createReducer } from '@reduxjs/toolkit'
 
 // Action creators
 export const addBug = createAction('ADD_BUG');
-export const removeBug = createAction('REMOVE_BUG');
+export const resolveBug = createAction('RESOLVE_BUG');
 
-// Reducer
 let lastId = 0;
 
-export default function reducer(state = [], action) {
-  switch (action.type) {
-    case addBug.type:
-      return [
-        ...state,
-        {
-          description: action.payload.description,
-          resolved: false,
-          id: ++lastId
-        }
-      ]
-      break;
+// Reducer
+export default createReducer([], {
+  [addBug.type]: (bugs, action) => {
+    bugs.push({
+      description: action.payload.description,
+      resolved: false,
+      id: ++lastId
+    })
+  },
 
-    case removeBug.type:
-      return state.filter(b => b.id !== action.payload.id)
-      break;
-
-    default:
-      return state;
-      break;
+  [resolveBug.type]: (bugs, action) => {
+    const index = bugs.findIndex(bug => bug.id === action.payload.id);
+    bugs[index].resolved = true
   }
-}
+});
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import store from "./02_FIRST_REDUX_APP/store";
-import { addBug, removeBug } from "./02_FIRST_REDUX_APP/bugs";
+import { addBug, resolveBug } from "./02_FIRST_REDUX_APP/bugs";
 
 store.dispatch(addBug({ description: 'New bug.' }))
 console.log(store.getState());
 
-store.dispatch(removeBug({ id: 1 }))
+store.dispatch(resolveBug({ id: 1 }))
 console.log(store.getState());
