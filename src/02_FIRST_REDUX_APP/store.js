955ff59fb2c76ab10264b798dcc6e71a32191276
charlesonunze <charlesonunze2@gmail.com>
2020-07-19 21:20:11 +0100

    feat: 🎸 add redux toolkit

--- src/02_FIRST_REDUX_APP/store.js
+++ src/02_FIRST_REDUX_APP/store.js
@@ -1,6 +1,6 @@
-import { createStore } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
 import reducer from './bugs';
 
-const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const store = configureStore({ reducer });
 
 export default store;
