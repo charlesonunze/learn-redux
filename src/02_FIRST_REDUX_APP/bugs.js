4064c606344b77501deaa64ac53d4b345237613c
charlesonunze <charlesonunze2@gmail.com>
2020-07-19 22:07:18 +0100

    feat: 🎸 redux-toolkit: createSlice!!!

--- src/02_FIRST_REDUX_APP/bugs.js
+++ src/02_FIRST_REDUX_APP/bugs.js
@@ -1,23 +1,26 @@
-import { createAction, createReducer } from '@reduxjs/toolkit'
-
-// Action creators
-export const addBug = createAction('ADD_BUG');
-export const resolveBug = createAction('RESOLVE_BUG');
+import { createSlice } from '@reduxjs/toolkit'
 
 let lastId = 0;
 
-// Reducer
-export default createReducer([], {
-  [addBug.type]: (bugs, action) => {
-    bugs.push({
-      description: action.payload.description,
-      resolved: false,
-      id: ++lastId
-    })
-  },
+// Bug Slice
+const bugSlice = createSlice({
+  name: 'bugs',
+  initialState: [],
+  reducers: {
+    addBug: (bugs, action) => {
+      bugs.push({
+        description: action.payload.description,
+        resolved: false,
+        id: ++lastId
+      })
+    },
 
-  [resolveBug.type]: (bugs, action) => {
-    const index = bugs.findIndex(bug => bug.id === action.payload.id);
-    bugs[index].resolved = true
+    resolveBug: (bugs, action) => {
+      const index = bugs.findIndex(bug => bug.id === action.payload.id);
+      bugs[index].resolved = true
+    }
   }
 });
+
+export const { addBug, resolveBug } = bugSlice.actions;
+export default bugSlice.reducer;
